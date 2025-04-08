704affed6026712ddc7a9a9a692ca18f7c82c772
Neelesh Kumar <neeleshega@gmail.com>
2025-04-08 10:31:31 +0530

    Assignments

--- src/com/cg/creatio/crm/language/basics/DebugTheProgram.java
+++ src/com/cg/creatio/crm/language/basics/DebugTheProgram.java
@@ -7,7 +7,7 @@ public class DebugTheProgram {
 		System.out.println("Step 2");
 		System.out.println("Step 3");
 		System.out.println("Step 4");
-		System.out.println("Step 5" + 5 / 4 + 7 + 3);
+		System.out.println("Step 5" + 5/(4+7+3));
 		System.out.println("Step 6");
 		System.out.println("Excecution Completed");
 	}
