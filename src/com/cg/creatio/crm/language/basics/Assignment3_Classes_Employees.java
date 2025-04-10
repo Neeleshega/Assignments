0f473c92f71df3dce61c357d4dc448a8a1bc32be
Neelesh Kumar <neeleshega@gmail.com>
2025-04-11 00:15:18 +0530

    Assignments

---
+++ src/com/cg/creatio/crm/language/basics/Assignment3_Classes_Employees.java
@@ -0,0 +1,14 @@
+package com.cg.creatio.crm.language.basics;
+
+public class Assignment3_Classes_Employees {
+
+		String empname1 = "Bharath";
+		String empname2 = "ABC";
+		String empname3 = "XYZ";
+
+		int empId1 = 1234;
+		int empId2 = 1235;
+		int empId3 = 1236;
+	}
+
+
